aef18f231a89e9e1eb23e0c936635af6904981d2
Davis Owen <davis.owen752@myci.csuci.edu>
2025-11-26 20:46:15 -0800

    Updating model to include ocean proximity

--- housing.py
+++ housing.py
@@ -24,9 +24,19 @@ def preprocess_data(df):
     longitude, latitude, median_income = df['longitude'].values, df['latitude'].values, df['median_income'].values
     std_longitude, std_latitude = np.std(longitude), np.std(latitude)
     mean_longitude, mean_latitude = np.mean(longitude), np.mean(latitude)
+    median_income = median_income * 1.233 # Scaling for modern income levels
     income_mean = median_income.mean()
     income_std  = median_income.std()
     ocean_proximity = df['ocean_proximity'].values
+    # Modifying ocean_proximity to numerical values
+    proximity_map = {
+        '<1H OCEAN': 0,
+        'INLAND': 1,
+        'NEAR OCEAN': 2,
+        'NEAR BAY': 3,
+        'ISLAND': 4
+    }
+    ocean_proximity_num = np.array([proximity_map[val] for val in ocean_proximity])
     
     Y = df['median_house_value'].values.reshape(-1, 1)
     Y = Y * 1.56 # Scaling by average house price increase over past 8 years
@@ -47,7 +57,7 @@ def preprocess_data(df):
     longitude = (longitude - mean_longitude) / std_longitude
     latitude = (latitude - mean_latitude) / std_latitude
     median_income = (median_income - income_mean) / income_std
-    X = np.column_stack((longitude, latitude, median_income))
+    X = np.column_stack((longitude, latitude, median_income, ocean_proximity_num))
     Y = (Y - Y_mean) / Y_std
     
     return df, X, Y, norm_stats
@@ -56,7 +66,7 @@ class HousingModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(3, 16),
+            nn.Linear(4, 16),
             nn.ReLU(),
             nn.Linear(16, 16),
             nn.ReLU(),
@@ -72,16 +82,16 @@ class HousingModel(nn.Module):
         return self.net(x)
         
 
-def predict_price(model, lat, lon, income, stats):
+def predict_price(model, lat, lon, income, prox, stats):
     # normalize inputs
     lon_norm = (lon - stats['lon_mean']) / stats['lon_std']
     lat_norm = (lat - stats['lat_mean']) / stats['lat_std']
     income_norm = (income - stats['income_mean']) / stats['income_std']
-    inp = torch.tensor([[lon_norm, lat_norm, income_norm]], dtype=torch.float32)
+    inp = torch.tensor([[lon_norm, lat_norm, income_norm, prox]], dtype=torch.float32)
+    inp = inp.to(next(model.parameters()).device)  # move to same device as model
 
     with torch.no_grad():  # no gradients during inference
         pred_norm = model(inp)
-
     # un-normalize output
     pred_price = pred_norm.item() * stats['Y_std'] + stats['Y_mean']
     return pred_price
@@ -94,14 +104,13 @@ def main():
 
     # Before starting on regression, I want to see if I can preemptively identify correlation with 
     # a heatmap, excluding categorical features
-    df_numeric = df.select_dtypes(include=[np.number, np.float64, np.int64])
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(df_numeric.corr(), annot=True, fmt=".2f")
-    plt.show()
-    
+    # df_numeric = df.select_dtypes(include=[np.number, np.float64, np.int64])
+    # plt.figure(figsize=(10, 8))
+    # sns.heatmap(df_numeric.corr(), annot=True, fmt=".2f")
+    # plt.show()
     # Previewing correlation shows little relevance between long / lat and median house value,
     # Though we might see better results with nonlinear regression.
-    # However, we do see strong correlation with number of rooms and bedrooms. Insightful.
+    # However, we do see strong correlation with number of rooms and number of bedrooms. Insightful.
     
     # Starting regression model
     X_tensor = torch.tensor(X, dtype=torch.float32)
@@ -112,12 +121,20 @@ def main():
     
     model = HousingModel()
     
-    criterion = nn.MSELoss() # MSE Loss for regression
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device:", device)
     
-    for epoch in range(500):
+    
+    criterion = nn.MSELoss() # MSE Loss for regression
+    optimizer = optim.Adam(model.parameters(), lr=0.005)
+    
+    model.to(device)
+    
+    for epoch in range(100):
         epochLoss = 0.0
         for batch_X, batch_Y in dataLoader:
+            batch_X = batch_X.to(device)
+            batch_Y = batch_Y.to(device)
             # Forward Pass
             predictions = model(batch_X)
             # Loss
@@ -134,12 +151,24 @@ def main():
     
     # Evaluation
     model.eval()
-    test_lon = -122.42183900
-    test_lat = 37.77378200
-    avg_income = 96.421
-    price = predict_price(model, test_lat, test_lon, avg_income, stats=norm_stats)
-    print("Estimated price:", price.floor())
+    # Calabasas, Bakersfield, Sacramento, Monterey
+    test_lon = [-118.51390200, -119.08364300, -121.47644300, -123.80421100]
+    test_lat = [34.20198200, 35.28472300, 38.53905900, 39.43978200]
+    med_income = [8.4628, 7.9355, 8.3753, 5.2051] # in tens of thousands
+    actual_prices = [700000, 575000, 419900, 399000]
+    proximities = [0,1,0,2] # <1H Ocean, Inland, <1H Ocean, Near Ocean
+    losses = []
+    
+    for i in range(4):
+        price = predict_price(model, test_lat[i], test_lon[i], med_income[i], proximities[i], stats=norm_stats)
+        print(f"Estimated price: ${price:,.0f}, actual price: ${actual_prices[i]:,.0f}")
+        losses.append((price - actual_prices[i])**2)
+    rmse = np.sqrt(np.mean(losses))
+    print(f"RMSE on test locations: ${rmse:,.0f}")
+        
 
+
+    
 
 if __name__ == "__main__":
     main()
